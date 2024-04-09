8f33d10faa62c65783d93a63ebb24a16dd865f86
Petunia Woldegerima <petuniatwg@gmail.com>
2024-04-09 11:06:51 -0400

    completed shoppinglist app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/shoppinglist.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class shoppinglist {
+    public static void main(String[] args) {
+        System.out.println("item 1");
+        System.out.println("item 2");
+        System.out.println("item 3");
+        System.out.println("item 4");
+        System.out.println("item 5");
+        System.out.println("item 6");
+        System.out.println("item 7");
+        System.out.println("item 8");
+        System.out.println("item 9");
+        System.out.println("item 10");
+    }
+}
