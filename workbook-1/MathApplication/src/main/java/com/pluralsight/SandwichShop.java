2c1bd0ffc30cf40b350900805d9b1ad69464c498
Petunia Woldegerima <petuniatwg@gmail.com>
2024-04-11 13:21:31 -0400

    added loaded option for the sandwich shop

--- workbook-1/MathApplication/src/main/java/com/pluralsight/SandwichShop.java
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/SandwichShop.java
@@ -4,28 +4,49 @@ import java.util.Scanner;
 
 public class SandwichShop {
     public static void main(String[] args) {
-        System.out.println("what kind of sandwich do you want Regular or Large");
+        System.out.println("what kind of sandwich do you want Regular , Large");
         Scanner scan=new Scanner(System.in);
         String sandwich= scan.nextLine();
+        System.out.println("do you want it loaded answer yes or no");
+        String ys= scan.nextLine();
         System.out.println("what is your age");
         int age=scan.nextInt();
 
 
         if (sandwich.equalsIgnoreCase("regular")){
+            if (ys.equalsIgnoreCase("no")){
             if (age<=17){
                 System.out.println( 5.45 -(5.45*0.1));}
             else if (age>=65){
                 System.out.println( 5.45-( 5.45*0.2));}
                 else {
-                    System.out.println(5.45);}
+                    System.out.println(5.45);}}
+            if (ys.equalsIgnoreCase("yes")){
+                if (age<=17){
+                System.out.println( 5.45 -(5.45*0.1)+1);}
+            else if (age>=65){
+                System.out.println( 5.45-( 5.45*0.2)+1);}
+            else {
+                System.out.println(5.45+1);}}
         }
             if (sandwich.equalsIgnoreCase("large")){
+                if (ys.equalsIgnoreCase("no")){
                 if (age<=17){
                     System.out.println( 8.95 -(8.95*0.1));}
                 else if (age>=65){
                     System.out.println( 8.95-(  8.95 *0.2));}
                 else {
-                    System.out.println(8.95);}
+                    System.out.println(8.95);}}
+
+                    if (ys.equalsIgnoreCase("yes")){
+                        if (age<=17){
+                            System.out.println( 8.95 -(8.95*0.1)+1.75);}
+                        else if (age>=65){
+                            System.out.println( 8.95-(  8.95 *0.2)+1.75);}
+                        else {
+                            System.out.println(8.95+1.75);}}
+
+
 
     }
 }}
