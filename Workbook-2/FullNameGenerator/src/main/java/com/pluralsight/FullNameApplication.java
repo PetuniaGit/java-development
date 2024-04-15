87069d123458585fecde542bae92ac2c1fbdaaf6
Petunia Woldegerima <petuniatwg@gmail.com>
2024-04-15 12:08:44 -0400

    third commit

--- Workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
+++ Workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 public class FullNameApplication {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        System.out.println("Enter your First name ,Middle initial , Last name and suffix.");
-        String Fullname= scan.nextLine();
+        System.out.println(" First name ");
+        String X= scan.nextLine();
+        System.out.println("middle name: enter N/A if you do not have one");
+        String Y=scan.nextLine();
+        System.out.println("last name");
+        String Z=scan.nextLine();
+        System.out.println("suffix: enter N/A if you do not have one");
+        String A= scan.nextLine();
+        String Fullname=" ";
+
+        if (Y.isEmpty()){
+            Fullname=X+" "+Z;
+        }
+        if (!(A.isEmpty())){
+            Fullname=Fullname+", "+A;
+        }
         Fullname=Fullname.trim();
         System.out.println(Fullname);
     }
