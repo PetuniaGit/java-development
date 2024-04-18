104013965c1c6e73e94b14d9ca9768b2d00d2299
Petunia Woldegerima <petuniatwg@gmail.com>
2024-04-18 13:58:35 -0400

    dice program

---
+++ Workbook-2/RollTheDice/src/main/java/com/pluralsight/Dice.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Dice {
+
+    public int roll(){
+     int randomNumber= (int) (Math.random()*6) +1;
+     return randomNumber;
+    }
+
+    public static void main(String[] args) {
+        int roll1=0;
+        int roll2=0;
+        int counter2= 0;
+        int counter4=0;
+        int counter6= 0;
+        int counter7=0;
+        Dice dice =new Dice();
+        for ( int i=1;i<=100;i++){
+            roll1=dice.roll();
+            roll2=dice.roll();
+            System.out.println("Roll"+ i +",sum=" + roll1 +roll2);
+            if (roll1+roll2==2){
+                counter2++;
+            }
+            if (roll1+roll2==4){
+                counter4++;
+            }
+            if ( roll1+roll2==6){
+                counter6++;
+            }
+            if ( roll1+roll2==7){
+                counter7++;
+            }
+        }
+        System.out.println(counter2);
+        System.out.println(counter4);
+        System.out.println(counter6);
+        System.out.println(counter7);
+
+    }
+}
