b07a61016b5d15ec84abcd5d3ef67778c5c431e7
Petunia Woldegerima <petuniatwg@gmail.com>
2024-05-06 12:24:10 -0400

    first commit

--- LearnToCode_capstones/AccountingLedger/src/main/java/AccountingLedger.java
+++ LearnToCode_capstones/AccountingLedger/src/main/java/AccountingLedger.java
@@ -8,6 +8,9 @@ import java.util.Scanner;
 
 public class AccountingLedger {
     static Scanner scan=new Scanner(System.in);
+    static LocalDateTime currentTime;
+    static DateTimeFormatter fmt;
+    static  String  DateTime;
 
     public static void homePage(){
         System.out.print("What would you like to do? ");
@@ -43,15 +46,15 @@ public class AccountingLedger {
         System.out.println("\nPlease enter the deposit information:");
 
         // Ask user to enter the deposit information.
-        System.out.print("Enter deposit description: ");
+        System.out.print(" Please enter deposit description: ");
         String depositDescription = scan.nextLine();
 
         // Ask user to enter deposit vendor.
-        System.out.print("Enter deposit vendor: ");
+        System.out.print("Please enter deposit vendor: ");
         String depositVendor = scan.nextLine();
 
         // Ask user to enter deposit amount.
-        System.out.print("Enter deposit amount: ");
+        System.out.print("Please enter deposit amount: ");
         double depositAmount = scan.nextDouble();
         scan.nextLine();
 
@@ -74,19 +77,100 @@ public class AccountingLedger {
         } catch (IOException e) {
             System.out.println("\nAn error occurred while writing to the file: " + e.getMessage() + "\n");
         }
+        homePage();
 
     }
     private static void makePayment() {
+        // Ask user for the payment informaiton.
+        System.out.println("\nPlease enter the payment information:");
+
+        // Ask user to enter the payment information.
+        System.out.print("Enter payment description: ");
+         String paymentDescription = scan.nextLine();
+
+        // Ask user to enter payment vendor.
+        System.out.print("Enter payment vendor: ");
+         String  paymentVendor = scan.nextLine();
+
+        // Ask user to enter payment amount.
+        System.out.print("Enter payment amount (as negative): ");
+          Double  paymentAmount = scan.nextDouble();
+        scan.nextLine();
+
+        // Get current date and time.
+        currentTime = LocalDateTime.now();
+
+        // Set the format for the date and time.
+        fmt= DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss");
+
+        // Format the date and time.
+        DateTime = currentTime.format(fmt);
+
+        // Write the payment information into the csv.
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.csv", true))) {
+            writer.write("\n" + DateTime + "|" + paymentDescription + "|" + paymentVendor + "|" + paymentAmount);
+
+            // Success messsage.
+            System.out.println("\nPayment information added to transactions.csv successfully.\n");
+            // If an error occured, print error.
+        } catch (IOException e) {
+            System.out.println("\nAn error occurred while writing to the file: " + e.getMessage() + "\n");
+        }
+
+        // Go back to home menu.
+        homePage();
     }
     private static void ledger() {
+        System.out.println("Enter 1 to display all transactions");
+        System.out.println("Enter 2 to display Deposits");
+        System.out.println("Enter 3 to display Payments");
+        System.out.println("Enter 4 to display Reports");
+        System.out.println("Enter 5 to go back to Home");
+
+        // Ask the user what they want to do.
+        System.out.print("Please select an option: ");
+         int  ledgerInput = scan.nextInt();
+
+        // If user chose A.
+        if (ledgerInput==1) {
+            // Call ledgerAll method.
+            ledgerAll();
+            // If user chose D.
+        } else if (ledgerInput==2) {
+            // Call ledgerDeposits method.
+            ledgerDeposits();
+            // If user chose P.
+        } else if (ledgerInput==3) {
+            // Call ledgerPayments method.
+            ledgerPayments();
+            // If user chose R.
+        } else if (ledgerInput==4) {
+            // Call ledgerReports method.
+            ledgerReports();
+            // If user chose H.
+        } else if (ledgerInput==5) {
+            // Return to home.
+            homePage();
+            // If user entered a wrong input.
+        } else {
+            System.out.print("Invalid input. Please try again: ");
+            ledgerInput = scan.nextInt();
+        }
     }
 
 
 
+    private static void ledgerReports() {
+    }
 
+    private static void ledgerPayments() {
+    }
 
+    private static void ledgerDeposits() {
+    }
 
-
+    private static void ledgerAll() {
+    }
 
 
 }
---
+++ Workbook-4/HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,53 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String Roomtype;
+    private int NumberofNights;
+    private boolean isWeekend;
+    private  double price;
+    public Reservation(String roomtype, int numberofNights, boolean isWeekend) {
+        Roomtype = roomtype;
+        NumberofNights = numberofNights;
+        this.isWeekend = isWeekend;
+    }
+    public String getRoomtype() {
+        return Roomtype;
+    }
+
+    public void setRoomtype(String roomtype) {
+        Roomtype = roomtype;
+    }
+
+    public int getNumberofNights() {
+        return NumberofNights;
+    }
+
+    public void setNumberofNights(int numberofNights) {
+        NumberofNights = numberofNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+  public double getPrice(){
+
+        if (Roomtype=="king"){
+            return price=139.00;
+        }
+else{
+      return price=124;}
+  }
+  public double getReservationTotal(){
+        if (isWeekend){
+        return price*NumberofNights;}
+        else{
+            return (price+price*0.1)*NumberofNights;
+        }
+  }
+
+}
