3111f99d385391b0873b96751c1eae738dfb51d7
Petunia Woldegerima <petuniatwg@gmail.com>
2024-05-21 11:12:44 -0400

    finished exercise

--- Workbook-6/WorkingwInterfaces/src/main/java/com/pluralsight/Person.java
+++ Workbook-6/WorkingwInterfaces/src/main/java/com/pluralsight/Person.java
@@ -1,14 +1,16 @@
 package com.pluralsight;
 
-public class Person implements Comparable {
+import java.util.Comparator;
+
+public class Person implements Comparable<Person> {
     private String firstName;
     private String lastName;
     private int age;
 
     public Person(String firstName, String lastName, int age) {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.age=age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
 
     public String getFirstName() {
@@ -36,25 +38,10 @@ public class Person implements Comparable {
     }
 
     @Override
-    public int compareTo(Object o) {
-        // Compare last names
-        if (this.lastName.compareTo(((Person) o).getLastName())==0) { // If the last names are the same
-            // Compare first names
-            if (this.firstName.compareTo(((Person) o).getFirstName()) == 0) { // If the first names are the same
-                // Compare age
-                if (this.age > ((Person) o).getAge()){
-                    return 1;
-                } else if (this.age==((Person) o).getAge()) {
-                    return 0;
-                } else {
-                    return -1;
-                }
-            } else {
-                return this.firstName.compareTo(((Person) o).getFirstName());
-            }
-        } else {
-            return this.lastName.compareTo(((Person) o).getLastName());
-        }
-
+    public int compareTo(Person other) {
+        return Comparator.comparing(Person::getLastName)
+                .thenComparing(Person::getFirstName)
+                .thenComparingInt(Person::getAge)
+                .compare(this, other);
     }
 }
--- Workbook-6/WorkingwInterfaces/src/main/java/com/pluralsight/WorkingWithInterfaces.java
+++ Workbook-6/WorkingwInterfaces/src/main/java/com/pluralsight/WorkingWithInterfaces.java
@@ -1,30 +1,28 @@
 package com.pluralsight;
 
-import com.pluralsight.Person;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
 public class WorkingWithInterfaces {
     public static void main(String[] args) {
-        List<Person> myFamily = new ArrayList<Person>();
-        myFamily.add( new Person("Dana", "Wyatt", 63) );
-        myFamily.add( new Person("Zachary", "Westly", 31) );
-        myFamily.add( new Person("Harriet", "Westly", 30) );
-        myFamily.add( new Person("Harriet", "Westly", 15) );
-        myFamily.add( new Person("Elisha", "Aslan", 14) );
-        myFamily.add( new Person("Elisha", "Aslan", 28) );
-        myFamily.add( new Person("Elisha", "Aslan", 18) );
-        myFamily.add( new Person("Elias", "Aslan", 14) );
-        myFamily.add( new Person("Ian", "Auston", 16) );
-        myFamily.add( new Person("Zephaniah", "Hughes", 9) );
-        myFamily.add( new Person("Ezra", "Aiden", 17) );
+        List<Person> people = new ArrayList<Person>();
+        people.add( new Person("Alice", "Johnson", 45) );
+        people.add( new Person("Brian", "Smith", 27) );
+        people.add( new Person("Charlotte", "Smith", 26) );
+        people.add( new Person("Charlotte", "Smith", 13) );
+        people.add( new Person("David", "Brown", 15) );
+        people.add( new Person("David", "Brown", 35) );
+        people.add( new Person("David", "Brown", 22) );
+        people.add( new Person("Daniel", "Brown", 15) );
+        people.add( new Person("Fiona", "White", 18) );
+        people.add( new Person("Grace", "Harris", 8) );
+        people.add( new Person("Henry", "Mason", 19) );
 
 
-        Collections.sort(myFamily);
+        Collections.sort(people);
 
-        for (Person person : myFamily) {
+        for (Person person : people) {
             System.out.println(person.getFirstName()+" "+person.getLastName()+" "+person.getAge());
         }
     }
