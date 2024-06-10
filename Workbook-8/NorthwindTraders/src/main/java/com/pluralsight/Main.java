9b25fc3bb5cfa9c9f2b75f8898a148e3751e72a4
Petunia Woldegerima <petuniatwg@gmail.com>
2024-06-10 13:58:12 -0400

    exercise1

---
+++ Workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // Load MySQL Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Open a connection to the database
+        Connection connection;
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root","Password345@");
+
+        // Create a statement
+        Statement statement = connection.createStatement();
+
+        // Define the query
+        String query = "SELECT productname FROM products";
+
+        // Execute query
+        ResultSet resultSet = statement.executeQuery(query);
+
+        // Process results
+        while (resultSet.next()) {
+            String product = resultSet.getString("productname");
+            System.out.println(product);
+        }
+
+        // Close connection
+        connection.close();
+    }
+}
