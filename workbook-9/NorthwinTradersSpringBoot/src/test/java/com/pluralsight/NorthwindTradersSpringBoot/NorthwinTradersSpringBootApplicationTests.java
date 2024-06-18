e1fac23757b805bf2cc27c9db55a96b677d4b029
Petunia Woldegerima <petuniatwg@gmail.com>
2024-06-18 08:52:55 -0400

    exercise done

--- workbook-9/NorthwinTradersSpringBoot/src/test/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwinTradersSpringBootApplicationTests.java
+++ workbook-9/NorthwinTradersSpringBoot/src/test/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwinTradersSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pluralsight.NorthwinTradersSpringBoot;
+package com.pluralsight.NorthwindTradersSpringBoot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
