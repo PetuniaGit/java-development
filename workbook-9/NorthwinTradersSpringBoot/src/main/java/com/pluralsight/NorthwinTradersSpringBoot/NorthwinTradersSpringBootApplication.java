1dc2f31be55d4f67e4f9984e5b1673ba044e8146
Petunia Woldegerima <petuniatwg@gmail.com>
2024-06-18 08:08:18 -0400

    exercise done

---
+++ workbook-9/NorthwinTradersSpringBoot/src/main/java/com/pluralsight/NorthwinTradersSpringBoot/NorthwinTradersSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwinTradersSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwinTradersSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwinTradersSpringBootApplication.class, args);
+	}
+
+}
---
+++ workbook-9/NorthwinTradersSpringBoot/src/test/java/com/pluralsight/NorthwinTradersSpringBoot/NorthwinTradersSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwinTradersSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwinTradersSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
